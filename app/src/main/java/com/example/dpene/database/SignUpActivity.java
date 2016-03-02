723aa3e89ed657e5ae728a240007aea9561d93e9
Donika Peneva <Donika Peneva>
2016-03-02 14:41:53 +0200

    Rename Sign up

--- app/src/main/java/com/example/dpene/database/SignUpActivity.java
+++ app/src/main/java/com/example/dpene/database/SignUpActivity.java
@@ -3,11 +3,11 @@ package com.example.dpene.database;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SingUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sing_up);
+        setContentView(R.layout.activity_sign_up);
     }
 }
