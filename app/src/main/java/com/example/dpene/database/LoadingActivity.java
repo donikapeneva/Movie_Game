7a1ea07f2fb7e412a35923ebed4c3d6310ff49da
Donika Peneva <Donika Peneva>
2016-03-07 09:10:10 +0200

    Reforming hogwarts letter layout

--- app/src/main/java/com/example/dpene/database/LoadingActivity.java
+++ app/src/main/java/com/example/dpene/database/LoadingActivity.java
@@ -3,9 +3,15 @@ package com.example.dpene.database;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.ProgressBar;
+
+import java.util.logging.Handler;
 
 public class LoadingActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private Handler handler;
+    private int progressStatus = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,10 +19,32 @@ public class LoadingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_loading);
 
 //        implementaciq za progressbar-a
+//        progressBar = (ProgressBar) findViewById(R.id.loading_page_progressbar);
 
+//        new Thread(new Runnable() {
+//            public void run() {
+//        while (true) {
+//            if (progressStatus < 100) {
+//                progressStatus += 1;
+//
+//                progressBar.setProgress(progressStatus);
+//
+//                try {
+//                    // Sleep for 200 milliseconds.
+//                    //Just to display the progress slowly
+//                    Thread.sleep(200);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            } else {
+//                Intent nextActivity = new Intent(this, LetterActivity.class);
+//                startActivity(nextActivity);
+//            }
+//        }
+//            }
+//        }).start();
+//    }
 
-        Intent nextActivity = new Intent(this, LetterActivity.class);
-        startActivity(nextActivity);
 
     }
 }
