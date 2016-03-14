15831c74484e86001e4434016630cfee5d389ae6
Ivelina Golemanova <ivelinagolemanova@gmail.com>
2016-03-14 23:53:09 +0200

    TODO File

---
+++ app/src/main/java/com/example/dpene/database/TODO/ThingsToDoAndOtherIdeas.java
@@ -0,0 +1,11 @@
+package com.example.dpene.database.TODO;
+
+/**
+ * Created by Golemanovaa on 14.3.2016 г..
+ */
+public class ThingsToDoAndOtherIdeas {
+
+    //TODO da zabranim back butona
+
+
+}
