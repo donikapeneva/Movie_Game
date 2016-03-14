52559ec4a33369bd26c02153b641452ddd26952e
Donika Peneva <Donika Peneva>
2016-03-15 01:54:50 +0200

    Winning activity and Fragment lost life

--- app/src/main/java/com/example/dpene/database/fragments/LoseLifeFragment.java
+++ app/src/main/java/com/example/dpene/database/fragments/LoseLifeFragment.java
@@ -1,32 +1,38 @@
 package com.example.dpene.database.fragments;
 
 
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.dpene.database.R;
+import com.example.dpene.database.model.PlayerManager;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LoseLifeFragment extends DialogFragment {
 
+    private Button continueButton;
 
     public LoseLifeFragment() {
         // Required empty public constructor
     }
 
-    @NonNull
+
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return super.onCreateDialog(savedInstanceState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int style = DialogFragment.STYLE_NO_TITLE;
+        int theme = android.R.style.Theme_Holo_Light_Dialog;
+
+        setStyle(style, theme);
     }
 
     @Override
@@ -38,7 +44,21 @@ public class LoseLifeFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lose_life, container, false);
+        View v = inflater.inflate(R.layout.fragment_lose_life, container, false);
+
+        continueButton = (Button) v.findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO implements interface communicator
+
+                //if player's hearts < 1 => SaveLifeActivity
+
+                //else go to next question
+            }
+        });
+
+        return v;
     }
 
 }
