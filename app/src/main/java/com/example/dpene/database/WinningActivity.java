50ca63e53f3ed1de3947aa824bcd890167aabf91
Donika Peneva <Donika Peneva>
2016-03-15 09:06:01 +0200

    Change imageButton to imageView in WinningAct

--- app/src/main/java/com/example/dpene/database/WinningActivity.java
+++ app/src/main/java/com/example/dpene/database/WinningActivity.java
@@ -5,19 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import java.util.Map;
 
 public class WinningActivity extends AppCompatActivity {
 
-    private ImageButton finishGame;
+    private ImageView finishGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_winning);
 
-        finishGame = (ImageButton) findViewById(R.id.win_button);
+        finishGame = (ImageView) findViewById(R.id.win_image);
         finishGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
