68ab1d7211de2c90b0e58b029acbf3a646e60e20
Donika Peneva <Donika Peneva>
2016-03-15 03:08:40 +0200

    Java code for Winning activity

--- app/src/main/java/com/example/dpene/database/WinningActivity.java
+++ app/src/main/java/com/example/dpene/database/WinningActivity.java
@@ -3,21 +3,27 @@ package com.example.dpene.database;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import java.util.Map;
 
 public class WinningActivity extends AppCompatActivity {
+
+    private ImageButton finishGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_winning);
 
-        //TODO the xml of the activity, some message perhaps
-
-        //get him to the entirely uncovered map when he taps on the screen
-        Intent nextActivity = new Intent(this, MapActivity.class);
-        startActivity(nextActivity);
+        finishGame = (ImageButton) findViewById(R.id.win_button);
+        finishGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WinningActivity.this, MapActivity.class));
+            }
+        });
 
     }
 }
