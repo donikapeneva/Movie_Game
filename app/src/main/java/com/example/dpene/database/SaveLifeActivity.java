96c9d2df0350ff330df33f0e793bcd3edd48a6a2
Donika Peneva <Donika Peneva>
2016-03-15 15:52:39 +0200

    Continue to LogicQActivity

--- app/src/main/java/com/example/dpene/database/SaveLifeActivity.java
+++ app/src/main/java/com/example/dpene/database/SaveLifeActivity.java
@@ -1,13 +1,26 @@
 package com.example.dpene.database;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SaveLifeActivity extends AppCompatActivity {
+
+    private Button tapToContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save_life);
+
+        tapToContinue = (Button) findViewById(R.id.continue_to_logic_question_button);
+        tapToContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SaveLifeActivity.this, LogicalQuestionActivity.class));
+            }
+        });
     }
 }
