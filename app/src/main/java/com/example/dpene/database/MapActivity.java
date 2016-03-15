e6b98370c93382fb98350591811fd864b2d0c1c9
Donika Peneva <Donika Peneva>
2016-03-15 22:59:20 +0200

    OnClick on levels in map

--- app/src/main/java/com/example/dpene/database/MapActivity.java
+++ app/src/main/java/com/example/dpene/database/MapActivity.java
@@ -1,15 +1,16 @@
 package com.example.dpene.database;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.dpene.database.model.PlayerManager;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements View.OnClickListener {
 
     private PlayerManager playerManager;
     private TextView level;
@@ -17,6 +18,10 @@ public class MapActivity extends AppCompatActivity {
     private ImageView heart2;
     private ImageView heart3;
     private ImageView levelHogwarts;
+    private ImageView pathHogwartsToPrison;
+    private ImageView levelPrison;
+    private ImageView pathPrisonToPirate;
+    private ImageView levelPirate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +37,16 @@ public class MapActivity extends AppCompatActivity {
         this.heart3 = (ImageView) findViewById(R.id.heart3);
 
         this.levelHogwarts = (ImageView) findViewById(R.id.hogwarts_castle);
-        levelHogwarts.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent nextActivity = new Intent(MapActivity.this, RegularQuestionActivity.class);
-                startActivity(nextActivity);
-            }
-        });
+        this.levelPrison = (ImageView) findViewById(R.id.prison_break);
+        this.levelPirate = (ImageView) findViewById(R.id.ship);
+
+        this.pathHogwartsToPrison = (ImageView) findViewById(R.id.path_to_prison);
+        this.pathPrisonToPirate = (ImageView) findViewById(R.id.path_to_ship);
+
+        levelHogwarts.setOnClickListener(this);
+        levelPrison.setOnClickListener(this);
+        levelPirate.setOnClickListener(this);
+
 
     }
 
@@ -47,10 +55,11 @@ public class MapActivity extends AppCompatActivity {
         super.onResume();
         showHearts();
         this.level.setText("Level " + playerManager.getLevel());
+
     }
 
-    private void showHearts(){
-        switch(playerManager.getLives()){
+    private void showHearts() {
+        switch (playerManager.getLives()) {
             case 3:
                 heart1.setVisibility(View.VISIBLE);
                 heart2.setVisibility(View.VISIBLE);
@@ -71,6 +80,34 @@ public class MapActivity extends AppCompatActivity {
                 heart2.setVisibility(View.INVISIBLE);
                 heart3.setVisibility(View.INVISIBLE);
                 break;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        int clickedLevel;
+        switch (v.getId()) {
+            default:
+            case R.id.hogwarts_castle:
+                clickedLevel = 1;
+                break;
+            case R.id.prison_break:
+                clickedLevel = 2;
+                break;
+            case R.id.ship:
+                clickedLevel = 3;
+                break;
+        }
+
+        int currentLevel = playerManager.getLevel();
+
+        if (clickedLevel < currentLevel) {
+            Toast.makeText(MapActivity.this, "You have already passed this level", Toast.LENGTH_SHORT).show();
+        } else if (clickedLevel > currentLevel) {
+            Toast.makeText(MapActivity.this, "You haven't reached that level yet", Toast.LENGTH_SHORT).show();
+        } else {
+            startActivity(new Intent(MapActivity.this, RegularQuestionActivity.class));
         }
     }
 }
