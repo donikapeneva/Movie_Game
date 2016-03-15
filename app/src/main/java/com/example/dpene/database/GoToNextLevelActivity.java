e7db82dc063165f9d876a581d8db634be81f7620
Ivelina Golemanova <ivelinagolemanova@gmail.com>
2016-03-15 23:33:11 +0200

    Create GoToNextLevelActivity

---
+++ app/src/main/java/com/example/dpene/database/GoToNextLevelActivity.java
@@ -0,0 +1,13 @@
+package com.example.dpene.database;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GoToNextLevelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_go_to_next_level);
+    }
+}
