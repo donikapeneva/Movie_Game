b68cd4480ae37ed419c9f1fedd5f946609a07e91
Ivelina Golemanova <ivelinagolemanova@gmail.com>
2016-03-15 18:07:09 +0200

    Async Task Started

---
+++ app/src/main/java/com/example/dpene/database/model/ShowNextQuestionTask.java
@@ -0,0 +1,9 @@
+package com.example.dpene.database.model;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Golemanovaa on 15.3.2016 г..
+ */
+public class ShowNextQuestionTask extends AsyncTask<> {
+}
