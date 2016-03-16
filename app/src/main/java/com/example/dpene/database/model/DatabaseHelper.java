d23699ce3bbc0685de628a7d3d03ca549a145eb8
Ivelina Golemanova <ivelinagolemanova@gmail.com>
2016-03-16 10:28:43 +0200

    App Ready I think

--- app/src/main/java/com/example/dpene/database/model/DatabaseHelper.java
+++ app/src/main/java/com/example/dpene/database/model/DatabaseHelper.java
@@ -11,7 +11,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static DatabaseHelper instance;
     
     private static final String DATABASE_NAME = "MOVIE_GAME_DATABASE";
-    private static final int DATABASE_VERSION = 19;
+    private static final int DATABASE_VERSION = 20;
 
     // tables
     public static final String TABLE_PLAYER = "player";
